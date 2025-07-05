ee897ea17c18f23608c3ae1d398f13f3b1e32f0d
vliguori <vliguori8@gmail.com>
2025-07-05 15:33:50 +0200

    Add creating new habit feature

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -7,18 +7,17 @@ const DashboardPage = () => {
   const [habits, setHabits] = useState([]);
   const [error, setError] = useState("");
 
+  const [newHabitName, setNewHabitName] = useState("");
+  const [newHabitDescription, setNewHabitDescription] = useState("");
+
   // react method that gets executed after the component is built
   useEffect(() => {
-    console.log("1. useEffect in Dashboard is getting executed...")
     const fetchHabits = async () => {
       try {
-        console.log("2. try loading habits. Authentication Data:", authData);
         if (authData && authData.accessToken) {
-          console.log("3. Token found: ", authData.accessToken);
           const userHabits = await habitService.getHabits(authData.accessToken);
           setHabits(userHabits);
         } else {
-          console.log("4. token not found")
         }
       } catch (error) {
         setError("Could not retrieve habits");
@@ -29,13 +28,59 @@ const DashboardPage = () => {
     fetchHabits();
   }, [authData]);
 
+  const handleCreateHabit = async (e) => {
+    e.preventDefault();
+    try {
+      const habitDto = { name: newHabitName, desription: newHabitDescription };
+      const newHabit = await habitService.createHabit(
+        habitDto,
+        authData.accessToken
+      );
+
+      // Refresh List of habit user
+      setHabits([...habits, newHabit]);
+      // Reset fields in the form
+      setNewHabitName("");
+      setNewHabitDescription("");
+    } catch (err) {
+      setError("Error during habit creation.");
+    }
+  };
+
   return (
     <div>
+      {/* Form for creating new Habit */}
+      <div className="create-habit-form">
+        <h3>Create new habit</h3>
+        <form onSubmit={handleCreateHabit}>
+          <div>
+            <label>Name:</label>
+            <input
+              type="text"
+              value={newHabitName}
+              onChange={(e) => setNewHabitName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Description:</label>
+            <input
+              type="text"
+              value={newHabitDescription}
+              onChange={(e) => setNewHabitDescription(e.target.value)}
+            />
+          </div>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+
+      <hr style={{ margin: "40px 0" }} />
+
       <h2>Your Habits</h2>
-      {error && <p style={{ color: 'red' }}></p>}
+      {error && <p style={{ color: "red" }}></p>}
       <ul>
         {habits.length > 0 ? (
-          habits.map(habit => <li key={habit.id}>{habit.name}</li>)
+          habits.map((habit) => <li key={habit.id}>{habit.name}</li>)
         ) : (
           <p> No Habits found. Create your first!</p>
         )}
--- src/services/habitService.js
+++ src/services/habitService.js
@@ -1,4 +1,4 @@
-const API_URL = "localhost://8080/api/v1/habits";
+const API_URL = "http://localhost:8080/api/v1/habits";
 
 /**
  * Function to retrieve the current user habits
@@ -21,8 +21,32 @@ const getHabits = (token) => {
   });
 };
 
+
+/**
+ * Funtion to create a new Habit
+ * @param {object} habitDto - the dto of the item to create
+ * @param {string} token - JWT token of the user
+ * @returns {Promise<object>} - the habit created
+ */
+const createHabit = (habitDto, token) => {
+  return fetch(API_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(habitDto)
+  }).then(response => { 
+    if (!response.ok) {
+      throw new Error('Could not create habit');
+    }
+    return response.json();
+  });
+};
+
 const habitService = {
   getHabits,
+  createHabit
 }
 
 export default habitService;
